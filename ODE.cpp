cf663703ae3e56d5c01ca149669fcfeebd4d84e3
vblandon10 <v.blandon10@uniandes.edu.co>
2018-11-19 10:25:26 -0500

    funcionesODE(nocorre)

--- ODE.cpp
+++ ODE.cpp
@@ -33,6 +33,14 @@ float derivada_vy(float vx, float vy)
 
 	// coeficiente de friccion c, gravedad y masa. todo en SI
 	float c=0.2, g=10, m=0.2;
+
+  float resultado = 0;
+	float norma_v;
+
+	norma_v = sqrt(vx*vx + vy*vy);
+	resultado = -g -c*(norma_v*norma_v/m)*(vy/norma_v);
+
+	return resultado;
 }
 
 
