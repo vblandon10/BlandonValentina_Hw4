7b92d272b55a0937f39ba11804fb13228daef01a
vblandon10 <v.blandon10@uniandes.edu.co>
2018-11-19 17:40:00 -0500

    graficas proyectiles

--- Plots_hw4.py
+++ Plots_hw4.py
@@ -16,3 +16,14 @@ print(i0, i1)
 figure()
 plot(proyectiles[i0:i1,0], proyectiles[i0:i1,2])
 savefig('p45.png')
+
+figure()
+for i in range(6):
+	i0 = int(ip[i])
+	i1 = int(ip[i+1])
+	plot(proyectiles[i0:i1,0], proyectiles[i0:i1,2], label=str((i+1)*10))
+i0=int(ip[-1])
+i1=-1
+plot(proyectiles[i0:i1,0], proyectiles[i0:i1,2], label=str(70))
+legend(loc='best')
+savefig('proyectiles.png')
