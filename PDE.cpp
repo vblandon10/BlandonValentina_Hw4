7ca47c4b56273247961081a9756d357f24b70b95
vblandon10 <v.blandon10@uniandes.edu.co>
2018-11-19 14:21:07 -0500

    PDE

--- PDE.cpp
+++ PDE.cpp
@@ -6,3 +6,24 @@
 using namespace std;
 
 int main(int argc, char const *argv[])
+{
+  // arg[1]=0 para condiciones fijas
+	// arg[1]=1 para condiciones periodicas
+	// arg[1]=2 para condiciones abiertas
+
+  // archivo en el que se va a scribir
+  ofstream escribir_datos(argv[2]);
+
+// Imprimir en la terminal el caso a tratar
+if (atoi(argv[1])==0)
+{
+  cout << "Condiciones de fronteras Fijas" << "\n\n";
+}else if (atoi(argv[1])==1)
+{
+  cout << "Condiciones de fronteras Abiertas" << "\n\n";
+}else if (atoi(argv[1])==2)
+{
+  cout << "Condiciones de fronteras periodicas" << "\n\n";
+}
+
+}
