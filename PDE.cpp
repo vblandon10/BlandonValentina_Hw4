51c9cf532b462b62ce3e265f0312e72fd4c6b65a
vblandon10 <v.blandon10@uniandes.edu.co>
2018-11-19 15:27:36 -0500

    PDE(malla)

--- PDE.cpp
+++ PDE.cpp
@@ -4,7 +4,7 @@
 #include "iostream"
 #include "fstream"
 using namespace std;
-
+//int atoi(const char *str) converts the string argument str to an integer (type int). (tomado de tutorials point)
 int main(int argc, char const *argv[])
 {
   // arg[1]=0 para condiciones fijas
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[])
   ofstream escribir_datos(argv[2]);
 
 // Imprimir en la terminal el caso a tratar
-//
+//int atoi(const char *str) converts the string argument str to an integer (type int).
 if (atoi(argv[1])==0)
 {
   cout << "Condiciones de fronteras Fijas" << "\n\n";
@@ -38,5 +38,26 @@ densidad = 2710;
 
 float coeficienteDifusion;
 coeficienteDifusion = conductividad/(calorEspecifico*densidad);
+
+// tamano de la malla
+int npuntos;
+npuntos = 80;
+
+float h;
+h = 50.0/(npuntos-1);
+// crear malla x,y con npuntos valores de 0 a 50.0
+int i,j;
+float X[npuntos][npuntos];
+float Y[npuntos][npuntos];
+
+// crear malla
+for (i = 0; i < npuntos; ++i)
+{
+  for (j = 0; j < npuntos; ++j)
+  {
+    X[i][j] = i*h;
+    Y[i][j] = j*h;
+  }
+}
 
 }
