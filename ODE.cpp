481a7f047e868105aef9986afd2da1cbfbdc69b6
vblandon10 <v.blandon10@uniandes.edu.co>
2018-11-19 09:46:58 -0500

    ODE(nocorre)

---
+++ ODE.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include "iostream"
+#include "fstream"
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+
+	float angulo=45.0;
+	calcular_proyectil(angulo);
+	/* code */
+	return 0;
+}
