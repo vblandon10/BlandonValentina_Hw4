a3e9e6e6e9474eabf2ebdd90acb35ad56c2a817b
vblandon10 <v.blandon10@uniandes.edu.co>
2018-11-19 10:01:21 -0500

    funcionesODE(nocorre)

--- ODE.cpp
+++ ODE.cpp
@@ -4,7 +4,29 @@
 #include "fstream"
 using namespace std;
 
+float derivada_x(float vx)
+{ // Funcion que devulve la derivada de la posicion x
+	return vx;
+}
 
+float derivada_y(float vy)
+{ // Funcion que devulve la derivada de la posicion y
+	return vy;
+}
+
+
+float derivada_vx(float vx, float vy)
+{ // Funcion que devulve la derivada de la velocidad x
+  // coeficiente de friccion c, gravedad y masa. todo en SI
+	float c=0.2, g=10, m=0.2;
+
+	float resultado = 0;
+	float norma_v;
+
+	norma_v = sqrt(vx*vx + vy*vy);
+	resultado = -c*(norma_v*norma_v/m)*(vx/norma_v);
+	return resultado;
+}
 int main(int argc, char const *argv[])
 {
 
